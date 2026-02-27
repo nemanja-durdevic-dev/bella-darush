9492374707496a5af736f497e2e12ce46fec0d57
nemanja-durdevic-dev <nemanja.durdevic.dev@gmail.com>
2026-02-27 22:27:32 +0100

    style: format import statements and fix trailing commas in migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,10 +1,10 @@
-import * as migration_20260208_204032 from './20260208_204032';
-import * as migration_20260209_110605 from './20260209_110605';
-import * as migration_20260226_181736_add_service_groups from './20260226_181736_add_service_groups';
-import * as migration_20260226_182825_add_sort_order_to_service_groups_and_services from './20260226_182825_add_sort_order_to_service_groups_and_services';
-import * as migration_20260226_183343_service_groups_many_to_many_with_service_order from './20260226_183343_service_groups_many_to_many_with_service_order';
-import * as migration_20260226_183956_remove_service_group_row_sort_order from './20260226_183956_remove_service_group_row_sort_order';
-import * as migration_20260227_120000_add_workers_description from './20260227_120000_add_workers_description';
+import * as migration_20260208_204032 from './20260208_204032'
+import * as migration_20260209_110605 from './20260209_110605'
+import * as migration_20260226_181736_add_service_groups from './20260226_181736_add_service_groups'
+import * as migration_20260226_182825_add_sort_order_to_service_groups_and_services from './20260226_182825_add_sort_order_to_service_groups_and_services'
+import * as migration_20260226_183343_service_groups_many_to_many_with_service_order from './20260226_183343_service_groups_many_to_many_with_service_order'
+import * as migration_20260226_183956_remove_service_group_row_sort_order from './20260226_183956_remove_service_group_row_sort_order'
+import * as migration_20260227_120000_add_workers_description from './20260227_120000_add_workers_description'
 
 export const migrations = [
   {
@@ -35,11 +35,11 @@ export const migrations = [
   {
     up: migration_20260226_183956_remove_service_group_row_sort_order.up,
     down: migration_20260226_183956_remove_service_group_row_sort_order.down,
-    name: '20260226_183956_remove_service_group_row_sort_order'
+    name: '20260226_183956_remove_service_group_row_sort_order',
   },
   {
     up: migration_20260227_120000_add_workers_description.up,
     down: migration_20260227_120000_add_workers_description.down,
     name: '20260227_120000_add_workers_description',
   },
-];
+]
