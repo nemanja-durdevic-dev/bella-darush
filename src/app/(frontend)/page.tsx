29a15736741ec52eec7d3430a2ca70f5cd0a1c1c
nemanja-durdevic-dev <nemanja.durdevic.dev@gmail.com>
2026-03-02 10:24:15 +0100

    style: update comment to clarify the purpose of the HomePage component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,8 +5,7 @@ import Header from './components/header'
 import Landing from './components/landing'
 import Services from './components/services'
 
-
-
+// This is the main page component for the home page of the application. It imports and renders the Header, Landing, About, Services, and Footer components to create a complete landing page for the website. The styles.css file is also imported to apply global styles to the page.
 export default async function HomePage() {
   return (
     <div>
